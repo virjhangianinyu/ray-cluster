5fa2845aa7137180340f4102ba73b378513a607f
Vir Jhangiani <virjhangianinyu>
2025-01-13 04:23:51 +0530

    adder svm experiments for hyperparameter tuning

---
+++ reserve_kvm/experiments/grid_search_tuning.py
@@ -0,0 +1,30 @@
+from ray import tune
+from ray.tune.tuner import Tuner
+from ray.air import RunConfig
+from common import svm_train
+
+# Grid Search Configuration
+grid_search_config = {
+    "kernel": tune.grid_search(["linear", "rbf", "poly"]),
+    "C": tune.grid_search([0.1, 1, 10]),
+    "gamma": tune.grid_search([0.01, 0.1, 1]),
+    "degree": tune.grid_search([2, 3, 4])  # Only for poly kernel
+}
+
+# Run Grid Search
+tuner = Tuner(
+    svm_train,
+    param_space=grid_search_config,
+    tune_config=tune.TuneConfig(num_samples=1),
+    run_config=RunConfig(
+        name="grid_search_svm",
+        storage_path="./ray_results"
+    )
+)
+
+# Run the tuning job
+results = tuner.fit()
+
+# Print the best hyperparameters
+best_result = results.get_best_result(metric="mean_loss", mode="min")
+print("Best hyperparameters:", best_result.config)
